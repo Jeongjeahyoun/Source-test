100f38ce4108f89d96ffda21ae41c50adbaee781
Jeongjeahyoun <zeni9@naver.com>
2016-07-19 00:28:56 +0900

    commit

---
+++ .metadata/.plugins/com.aptana.ruby.core/2045711004/4/constants.rb
@@ -0,0 +1,13 @@
+ARGV = []
+TRUE = true
+FALSE = false
+STDOUT = IO.new
+STDIN = IO.new
+STDERR = IO.new
+ENV = {}
+NIL = nil
+RUBY_PATCHLEVEL = 112
+RUBY_COPYRIGHT = 'ruby - Copyright (C) 1993-2016 Yukihiro Matsumoto'
+TOPLEVEL_BINDING = Binding.new
+RUBY_VERSION = '2.3.1'
+RUBY_PLATFORM = 'x86_64-darwin15'
